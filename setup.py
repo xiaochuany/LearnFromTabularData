283bb4bb06fd85de9193763d467ff180b9d1570f
xiaochuany <xiaochuan.j.yang@gmail.com>
2023-09-21 10:07:09 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='create a pipeline for EDA and prediction for tabular data',
+    author='Xiaochuan Yang',
+    license='MIT',
+)
